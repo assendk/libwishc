3d43e1282d26a3631b450a0e11b3bc50f69c9fd0
assendk <assendk@gmail.com>
2023-04-26 20:23:37 +0300

    debug

--- src/Controller/PackageController.php
+++ src/Controller/PackageController.php
@@ -11,13 +11,13 @@ use Symfony\Component\Serializer\Encoder\JsonEncode;
 class PackageController extends AbstractController
 {
     /**
-     * @Route("/package/{platform}/{name}", name="package_details", methods={"GET"})
+     * @Route("/package/{platform}/{name<[^/]+>}", name="package_details", methods={"GET"})
      */
     public function showPackage($platform, $name, LibrariesIoApi $librariesIoApi)
     {
         $apiKey = $this->getParameter('libraries_io_api_key');
         $platform = strtolower(filter_var($platform, FILTER_SANITIZE_STRING));
-        $name = str_replace('/', '_', urldecode(filter_var($name, FILTER_SANITIZE_STRING)));
+//        $name = str_replace('/', '_', urldecode(filter_var($name, FILTER_SANITIZE_STRING)));
 
         $result = $librariesIoApi->getPackageDetails($platform, $name);
 
